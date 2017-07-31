e73bb8db71c0988c6197328cb0f1069eed5457c5
Guido <guido@vonrudorff.de>
2017-07-31 12:20:40 +0100

    Added prompt

--- guido/bashinclude.sh
+++ guido/bashinclude.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 BASEDIR=$(dirname $(realpath "$BASH_SOURCE" ))
+
+# Prompt
+PS1='\[\e[37m\]$HCHAIN \[\e[32m\]\[\e[31;1m\]\h\[\e[37m\]:\[\e[32m\]\w \$\[\e[0m\] ' 
+PS2="\u@\h:\w> "
+
+# Pretty git log
+alias gitlog="git log --oneline --abbrev-commit --all --graph --decorate --color" 
+
+# Disable bell
+setterm -blength 0 &> /dev/null
+
+# Read SSH config
 alias ssh='ssh -F "$BASEDIR/ssh-config"'
